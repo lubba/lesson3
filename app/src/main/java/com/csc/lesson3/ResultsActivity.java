354cfa8c8a78cc8d0ff04f6039667eac23998361
Paina Liubov <lubashka.994@mail.ru>
2016-03-11 19:56:31 +0300

    Add onClickListener

--- app/src/main/java/com/csc/lesson3/ResultsActivity.java
+++ app/src/main/java/com/csc/lesson3/ResultsActivity.java
@@ -1,13 +1,21 @@
 package com.csc.lesson3;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class ResultsActivity extends Activity {
+
+    private static final String TAG = "ResultsActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        Intent intent = getIntent();
+        String query = intent.getStringExtra("SEARCH_QUERY");
+        Log.d(TAG, "onCreate: Received query: " + query);
     }
 }
--- app/src/main/java/com/csc/lesson3/SearchActivity.java
+++ app/src/main/java/com/csc/lesson3/SearchActivity.java
@@ -1,13 +1,47 @@
 package com.csc.lesson3;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SearchActivity extends Activity {
+
+    private static final String TAG = "SearchActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        final Button searchButton = (Button) findViewById(R.id.search_action);
+        final EditText searchLine = (EditText) findViewById(R.id.search_line);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = searchLine.getText().toString();
+
+                text = text.trim().toLowerCase();
+                Log.d(TAG, "onClick: Text = " + text);
+                if (check(text)) {
+                    Intent intent = new Intent(getBaseContext(), ResultsActivity.class);
+                    intent.putExtra("SEARCH_QUERY", text);
+                    startActivity(intent);
+                } else {
+                    Toast toast = Toast.makeText(getBaseContext(), R.string.query_check_error, Toast.LENGTH_LONG);
+                    toast.show();
+                    Log.e(TAG, "onClick: check returned false");
+                }
+            }
+        });
+    }
+
+    private boolean check(String query) {
+        return !query.contains(" ");
     }
 }
