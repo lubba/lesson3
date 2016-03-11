bc904d6c428e70cb57a50daa99fb78abca67a5da
Paina Liubov <lubashka.994@mail.ru>
2016-03-11 19:10:01 +0300

    Add activity and rename old

--- app/src/main/java/com/csc/lesson3/ResultsActivity.java
+++ app/src/main/java/com/csc/lesson3/ResultsActivity.java
@@ -3,10 +3,11 @@ package com.csc.lesson3;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class ResultsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
     }
 }
---
+++ app/src/main/java/com/csc/lesson3/SearchActivity.java
@@ -0,0 +1,13 @@
+package com.csc.lesson3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SearchActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
