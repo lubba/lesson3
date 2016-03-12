dc23e0b0451c55a870fcf03ece9f4b086db36b89
lpaina <lubashka.994@mail.ru>
2016-03-12 23:32:07 +0300

    Add recycler and card views

---
+++ app/src/main/java/com/csc/lesson3/RVAdapter.java
@@ -0,0 +1,55 @@
+package com.csc.lesson3;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class RVAdapter extends RecyclerView.Adapter<RVAdapter.PersonViewHolder> {
+
+    public static class PersonViewHolder extends RecyclerView.ViewHolder {
+        CardView cardView;
+        TextView textView;
+        ImageView imageView;
+
+        PersonViewHolder(View itemView) {
+            super(itemView);
+            cardView = (CardView) itemView.findViewById(R.id.card_view);
+            textView = (TextView) itemView.findViewById(R.id.text_view);
+            imageView = (ImageView) itemView.findViewById(R.id.image_view);
+        }
+    }
+
+    List<Person> persons;
+
+    RVAdapter(List<Person> persons) {
+        this.persons = persons;
+    }
+
+    @Override
+    public int getItemCount() {
+        return persons.size();
+    }
+
+    @Override
+    public PersonViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.recyclerview_item, viewGroup, false);
+        return new PersonViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(PersonViewHolder personViewHolder, int i) {
+        personViewHolder.textView.setText(persons.get(i).name);
+        personViewHolder.imageView.setImageResource(persons.get(i).photoId);
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+}
--- app/src/main/java/com/csc/lesson3/ResultsActivity.java
+++ app/src/main/java/com/csc/lesson3/ResultsActivity.java
@@ -3,7 +3,12 @@ package com.csc.lesson3;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ResultsActivity extends Activity {
 
@@ -17,5 +22,20 @@ public class ResultsActivity extends Activity {
         Intent intent = getIntent();
         String query = intent.getStringExtra("SEARCH_QUERY");
         Log.d(TAG, "onCreate: Received query: " + query);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        List<Person> persons = new ArrayList<>();
+        persons.add(new Person("Emma Wilson", "23 years old", R.drawable.smile_icon));
+        persons.add(new Person("Lavery Maiss", "25 years old", R.drawable.smile_icon));
+        persons.add(new Person("Lillie Watts", "35 years old", R.drawable.smile_icon));
+
+        recyclerView.setAdapter(new RVAdapter(persons));
+
     }
+
+
 }
